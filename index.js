1adcff31be3a49a517c6d0c872b380f9e8731eca
kinomipolo-maker <kinomipolo@gmail.com>
2026-03-27 23:16:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-export default defineComponent({
-  async run({ steps, $ }) {
-    
-    // Генерируем случайное число от -20 до 30
-    const size = Math.floor(Math.random() * 51) - 20;
-    const absSize = Math.abs(size);
-    
-    // Формируем сообщение
-    let message = `, твоя биба: ${absSize} см! `;
-    
-    if (size <= -10) message += "Пупупу, в другой раз повезет?!";
-    else if (size <= 0) message += "Эй, ты! Да, ты! сожалею тебе!";
-    else if (size <= 5) message += "Ха-ха! Ну хоть что-то!";
-    else if (size <= 10) message += "Ну… хотя бы не минус. Та и тут вроде холодно!";
-    else if (size <= 20) message += "Мужик, ты валыну убери!";
-    else if (size <= 25) message += "Сожалею, неудобно наверное...";
-    else message += "Ого! Ты сбил счетчик!";
-    
-    // Возвращаем результат
-    return message;
+export default function handler(req, res) {
+  const size = Math.floor(Math.random() * 51) - 20;
+  const absSize = Math.abs(size);
+
+  let message = `, твоя биба: ${absSize} см! `;
+
+  if (size <= -10) {
+    message += "Пупупу, в другой раз повезёт?!";
+  } else if (size <= 0) {
+    message += "Эй, ты! Да, ты! сожалею тебе!";
+  } else if (size <= 5) {
+    message += "Ха-ха! Ну хоть что-то!";
+  } else if (size <= 10) {
+    message += "Ну… хотя бы не минус. Та и тут вроде холодно!";
+  } else if (size <= 20) {
+    message += "Мужик, ты валыну убери!";
+  } else if (size <= 25) {
+    message += "Сожалею, неудобно наверное...";
+  } else {
+    message += "Ого! Ты сбил счётчик!";
   }
-})
+
+  res.setHeader("Content-Type", "text/plain; charset=utf-8");
+  res.status(200).send(message);
+}
